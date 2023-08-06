450b0e8648cae6c44638a225196ad998e4c2c4e7
Mohammadabutaimeh <hamota741994@gmail.com>
2023-08-06 20:56:45 +0300

    Update : remove the gender  popup message

--- app.js
+++ app.js
@@ -3,20 +3,10 @@ if (StudentName=="") {
     alert(`you have to enter your name !! `);  
     StudentName=prompt('Please enter your name');
 }
-let StudentGender = prompt('Please enter your gender!\n(male or female)').toLowerCase();
-switch(StudentGender){
-    case 'male':
-        StudentMark = prompt(`Welcome Mr ${StudentName}.\nPlease enter your mark !`)
-        result =Math.floor(StudentMark)
-        break;
-    case 'female':
-        StudentMark = prompt(`Welcome Ms ${StudentName}.\nPlease enter your mark !`)
-        result =Math.floor(StudentMark)
-        break;
-    default:
-        StudentMark= prompt(`Welcome ${StudentName}.\nPlease enter your mark !`)
-        result =Math.floor(StudentMark)
-}
+
+        StudentMark = prompt(`Welcome ${StudentName}.\nPlease enter your mark !`)
+        result = Math.floor(StudentMark)
+       
 
 
 if (result<0) {
@@ -24,19 +14,8 @@ if (result<0) {
        
         alert(`The mark ${StudentMark} is less than zero !! `);  
      
-        switch(StudentGender){
-         case 'male':
-             StudentMark = prompt(`Welcome Mr ${StudentName}.\nPlease enter your mark again!`)
-             result =Math.floor(StudentMark)
-             break;
-         case 'female':
-             StudentMark = prompt(`Welcome Ms ${StudentName}.\nPlease enter your mark again!`)
-             result =Math.floor(StudentMark)
-             break;
-         default:
-             StudentMark= prompt(`Welcome ${StudentName}.\nPlease enter your mark again!`)
-             result =Math.floor(StudentMark)
-     }
+        StudentMark = prompt(`Welcome ${StudentName}.\nPlease enter your mark again!`)
+        result = Math.floor(StudentMark)
         
         
   
@@ -44,45 +23,23 @@ if (result<0) {
 
 if (result>100) {
     alert(`The mark ${StudentMark} is more than 100 !! `);  
-    switch(StudentGender){
-        case 'male':
-            StudentMark = prompt(`Welcome Mr ${StudentName}.\nPlease enter your mark again!`)
-            result =Math.floor(StudentMark)
-            break;
-        case 'female':
-            StudentMark = prompt(`Welcome Ms ${StudentName}.\nPlease enter your mark again!`)
-            result =Math.floor(StudentMark)
-            break;
-        default:
-            StudentMark= prompt(`Welcome ${StudentName}.\nPlease enter your mark again!`)
-            result =Math.floor(StudentMark)
-    }
+    StudentMark = prompt(`Welcome ${StudentName}.\nPlease enter your mark again!`)
+        result  = Math.floor(StudentMark)
        
  }
 
 
  if (result=="") {
     alert(`The mark field is empty mustn't be Null !! `);  
-    switch(StudentGender){
-        case 'male':
-            StudentMark = prompt(`Welcome Mr ${StudentName}.\nPlease enter your mark again!`)
-            result =Math.floor(StudentMark)
-            break;
-        case 'female':
-            StudentMark = prompt(`Welcome Ms ${StudentName}.\nPlease enter your mark again!`)
-            result =Math.floor(StudentMark)
-            break;
-        default:
-            StudentMark= prompt(`Welcome ${StudentName}.\nPlease enter your mark again!`)
-            result =Math.floor(StudentMark)
-    }
+    StudentMark = prompt(`Welcome ${StudentName}.\nPlease enter your mark again!`)
+        result =Math.floor(StudentMark)
        
  }
 
 
 
 if (result>=0 && result<=49) {
-    alert(`The mark ${StudentMark} is equal to the academic grade ( F )\n YOU FAILED\nBetter luck next time!`);  
+    alert(`The mark ${StudentMark} is equal to the academic grade ( F )\n\n                             YOU FAILED \n\n                      Better luck next time!`);  
 } else{
 
 if (result>=50 && result<=60) {
