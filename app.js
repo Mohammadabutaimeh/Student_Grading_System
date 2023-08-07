38a4f0e891530be9bbbde9c8cc40547a2b4b2132
Mohammadabutaimeh <hamota741994@gmail.com>
2023-08-07 22:55:20 +0300

    Update : update the function of descover numeric and non numeric value

--- app.js
+++ app.js
@@ -7,23 +7,13 @@ if (StudentName=="" ||StudentName==" " || StudentName=="  ") {
     while (StudentName=="" ||StudentName==" " || StudentName=="  ");
   }
 
-
-function isNumber(str) {
-    return !!( 
-      str && 
-      str.match(/^\d+$/)); 
-  }
-  if (isNumber(StudentName)) {
-    do {
-      alert('Wrong,please try again');
-      StudentName=prompt('Please enter your name');
-    }
-    while (isNumber(StudentName));
-  }
+  
+    
   
 if (StudentName != null) {
-  
 
+  if (isNaN(StudentName)) {
+   
         StudentMark = prompt(`Welcome ${StudentName}.\nPlease enter your mark !`)
         result = Math.floor(StudentMark)
        
@@ -57,7 +47,16 @@ if (StudentName != null) {
             }
             while (StudentMark=="" || StudentMark==" " || StudentMark=="  ");
           }
-if (StudentMark != null) {
+          if (isNaN(StudentMark)) {
+            do {
+              alert("Invalid input! Please enter a numeric value."); 
+                StudentMark = prompt(`Welcome ${StudentName}.\nPlease enter your mark again!`)
+                result = Math.floor(StudentMark)
+            }
+            while (isNaN(StudentMark));
+          }
+
+if (StudentMark != null  ) {
 
 if (result>=0 && result<=49) {
     alert(`The mark ${StudentMark} is equal to the academic grade ( F )\n\n                             YOU FAILED \n\n                      Better luck next time!`);  
@@ -68,6 +67,90 @@ if (result>=50 && result<=60) {
 }
 }
 
+if (result>=61 && result<=70) {
+    alert(`The mark ${StudentMark} is equal to the academic grade ( C )\n\n                             YOU PASSED \n\n                               Congrats!`);  
+} else{
+
+if (result>=71 && result<=80) {
+    alert(`The mark ${StudentMark} is equal to the academic grade ( B )\n\n                             YOU PASSED \n\n                               Congrats!`);  
+}
+}
+
+if (result>=81 && result<=90) {
+    alert(`The mark ${StudentMark} is equal to the academic grade ( A )\n\n                             YOU PASSED \n\n                               Congrats!`);  
+} else{
+
+if (result>=91 && result<=100) {
+    alert(`The mark ${StudentMark} is equal to the academic grade ( A+ )\n\n                             YOU PASSED \n\n                               Congrats!`);  
+}
+}
+
+}
+
+  
+}  
+
+  
+
+else {
+  do {
+    alert("Invalid input! Please enter a non-numeric value.");
+    StudentName=prompt('Please enter your name');
+
+    }
+    while (!isNaN(StudentName) & StudentName!=null)
+    if (StudentName != null){
+    StudentMark = prompt(`Welcome ${StudentName}.\nPlease enter your mark !`)
+        result = Math.floor(StudentMark)
+       
+
+        if (result<0) {
+            do {
+                alert(`The mark ${StudentMark} is less than zero !! `);  
+     
+                StudentMark = prompt(`Welcome ${StudentName}.\nPlease enter your mark again!`)
+                result = Math.floor(StudentMark)
+            }
+            while (result<0);
+          }
+    
+          
+          if (result>100) {
+            do {
+                alert(`The mark ${StudentMark} is more than 100 !! `);  
+     
+                StudentMark = prompt(`Welcome ${StudentName}.\nPlease enter your mark again!`)
+                result = Math.floor(StudentMark)
+            }
+            while (result>100);
+          }
+
+          if (StudentMark=="" || StudentMark==" " || StudentMark=="  ") {
+            do {
+                alert(`The mark Field is empty please enter your mark !! `); 
+                StudentMark = prompt(`Welcome ${StudentName}.\nPlease enter your mark again!`)
+                result = Math.floor(StudentMark)
+            }
+            while (StudentMark=="" || StudentMark==" " || StudentMark=="  ");
+          }
+          if (isNaN(StudentMark)) {
+            do {
+              alert("Invalid input! Please enter a numeric value."); 
+                StudentMark = prompt(`Welcome ${StudentName}.\nPlease enter your mark again!`)
+                result = Math.floor(StudentMark)
+            }
+            while (isNaN(StudentMark));
+          }
+if (StudentMark != null ) {
+
+if (result>=0 && result<=49) {
+    alert(`The mark ${StudentMark} is equal to the academic grade ( F )\n\n                             YOU FAILED \n\n                      Better luck next time!`);  
+} else{
+
+if (result>=50 && result<=60) {
+    alert(`The mark ${StudentMark} is equal to the academic grade ( D )\n\n                             YOU PASSED \n\n                               Congrats!`);  
+}
+}
 
 if (result>=61 && result<=70) {
     alert(`The mark ${StudentMark} is equal to the academic grade ( C )\n\n                             YOU PASSED \n\n                               Congrats!`);  
@@ -78,8 +161,6 @@ if (result>=71 && result<=80) {
 }
 }
 
-
-
 if (result>=81 && result<=90) {
     alert(`The mark ${StudentMark} is equal to the academic grade ( A )\n\n                             YOU PASSED \n\n                               Congrats!`);  
 } else{
@@ -88,5 +169,6 @@ if (result>=91 && result<=100) {
     alert(`The mark ${StudentMark} is equal to the academic grade ( A+ )\n\n                             YOU PASSED \n\n                               Congrats!`);  
 }
 }
+
 }
-}
+}}}
